543921bca61b1f4b084cf89ba83e9ff657efcb56
“zayar <barblack2842@gmail.com>
2024-01-10 19:54:51 +0630

    image explain

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import styles from "./about.module.css"
+
 export default function About() {
     return (
         <>
-            <div>about</div>
+            <div className={styles.container}>
+                <Image src="/about.png" alt="" fill/>
+            </div>
         </>
     );
 }
