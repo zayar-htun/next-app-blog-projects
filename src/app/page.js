76c8c3e64a12ce770353c56044d9ad493491c2c3
“zayar <barblack2842@gmail.com>
2024-01-10 20:28:22 +0630

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,39 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 export default function Home() {
     return (
         <>
-            <div>home</div>
+            <div className={styles.container}>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.title}>Creative Thoughts Agency</h1>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        In quis suscipit enim. Donec et vehicula eros, nec
+                        accumsan risus. Fusce scelerisque nulla sit amet sapien
+                        hendrerit lacinia. 
+                    </p>
+                    <div className={styles.buttons}>
+                        <button className={styles.button}>Learn More</button>
+                        <button className={styles.button}>Contact</button>
+                    </div>
+                    <div className={styles.brands}>
+                        <Image
+                            src="/brands.png"
+                            alt=""
+                            fill
+                            className={styles.brandImage}
+                        />
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="/hero.gif"
+                        alt=""
+                        fill
+                        className={styles.heroImage}
+                    />
+                </div>
+            </div>
         </>
     );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,3 +1,12 @@
+import styles from "./footer.module.css";
+
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>Zayar Dev</div>
+            <div className={styles.text}>
+                Trojan creative thoughts agency © All rights reserved.
+            </div>
+        </div>
+    );
 }
